66c2e45b394c0783e2e235cbe2f40049ef665b26
Роман Смирнов <pombl4.01@mail.ru>
2022-10-17 12:47:53 +0300

    fixed bug with fusion of molecules

--- GasSimulation/Molecule.h
+++ GasSimulation/Molecule.h
@@ -6,12 +6,14 @@ class Molecule {
 private:
 	sf::CircleShape shape;
 	sf::Vector2f velocity;	// value - pixels in second
+	sf::RectangleShape move_vector;
 
 public:
 	Molecule();
 	Molecule(int p_x, int p_y, int radius, double vel_x, double vel_y);
 
 	void update(double& dt_time);
+	void updateMoveVector();
 	void setVelocity(sf::Vector2f vel);
 	sf::CircleShape& getShape();
 	void setPos(sf::Vector2f pos);
--- GasSimulation/main.cpp
+++ GasSimulation/main.cpp
@@ -7,14 +7,25 @@ int main()
 {
     srand(time(NULL));
     Renderer rend(screen_width, screen_heigth);
-    for (int i = 0; i < molecules_size; i++) {
+    int prev_x = wall_size + 10;
+    int prev_y = wall_size + 10;
+    int prev_rad = 0;
+    // many magic variables but i dont care
+    for (int i = 0; i < molecules_size / 5; i++) {
         int vel_x = rand() % max_velocity;
         int vel_y = rand() % max_velocity;
-        Molecule mol1(
-            (rand() % (int)(screen_width * 0.80)) + (int)(screen_width * 0.10), 
-            (rand() % (int)(screen_heigth * 0.80)) + (int)(screen_heigth * 0.10), 
-            10, vel_x, vel_y);
-        rend.addMolecule(mol1);
+        for (int j = 0; j < 5; j++) {
+            int distance = prev_rad * 2 + 30;
+            int radius = rand() % 10 + 5;
+            int p_x = prev_x + distance + 30;
+            int p_y = prev_y;
+            Molecule mol(p_x, p_y, radius, vel_x, vel_y);
+            rend.addMolecule(mol);
+            prev_x = p_x;
+            prev_rad = radius;
+        }
+        prev_x = wall_size + 10;
+        prev_y = prev_y + 40;
     }
 
     rend.run();
